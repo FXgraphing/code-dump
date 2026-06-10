2aaebf55518803890e1e2e063feacb50e8ccc10c
FXgraphing <fxgraphing@atomicmail.io>
2026-06-10 19:43:49 +0200

    Add ipv4_calculator.py

---
+++ ipv4_calculator.py
@@ -0,0 +1,29 @@
+octet1 = int(input("Enter the first octet (0-255): "))
+octet2 = int(input("Enter the second octet (0-255): "))
+octet3 = int(input("Enter the third octet (0-255): "))
+octet4 = int(input("Enter the fourth octet (0-255): "))
+prefix = int(input("Enter the prefix (0-32): "))
+
+address_count = 2 ** (32 - prefix)
+host_count = address_count - 2
+mask = (2 ** 32) - address_count
+mask_octet1 = (mask >> 24) & 0xFF
+mask_octet2 = (mask >> 16) & 0xFF
+mask_octet3 = (mask >> 8) & 0xFF
+mask_octet4 = mask & 0xFF
+network_octet1 = octet1 & mask_octet1
+network_octet2 = octet2 & mask_octet2
+network_octet3 = octet3 & mask_octet3
+network_octet4 = octet4 & mask_octet4
+broadcast_octet1 = network_octet1 | (255 - mask_octet1)
+broadcast_octet2 = network_octet2 | (255 - mask_octet2)
+broadcast_octet3 = network_octet3 | (255 - mask_octet3)
+broadcast_octet4 = network_octet4 | (255 - mask_octet4)
+
+print(f"IPv4: {octet1}.{octet2}.{octet3}.{octet4}/{prefix}")
+print("============")
+print(f"Network address: {network_octet1}.{network_octet2}.{network_octet3}.{network_octet4}")
+print(f"Broadcast address: {broadcast_octet1}.{broadcast_octet2}.{broadcast_octet3}.{broadcast_octet4}")
+print(f"Subnet mask: {mask_octet1}.{mask_octet2}.{mask_octet3}.{mask_octet4}")
+print(f"Address count: {address_count}")
+print(f"Host count: {host_count}")
